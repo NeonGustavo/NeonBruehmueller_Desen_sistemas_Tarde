5f1deb077700c51386f2e705131c70fb7ba4773d
NeonGustavo <Neon.top75@gmail.com>
2025-05-20 17:08:27 -0300

    Principal

--- Neon_Bruehmueller.php/MaterialApoio/principal.php
+++ Neon_Bruehmueller.php/MaterialApoio/principal.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['usuario'])) {
     header("location:login.php");
     exit();
 
- //OBTENDO O NOME DO PERFIL DO USUÁRIO
+    //OBTENDO O NOME DO PERFIL DO USUÁRIO
     $id_perfil = $_SESSION['perfil'];
     $sql = "SELECT nome FROM perfil WHERE id_perfil = :id_perfil";
     $stmt = $pdo->prepare($sql);
@@ -19,7 +19,7 @@ if (!isset($_SESSION['usuario'])) {
     $id_perfil = $_SESSION['perfil'];
     $sqlPerfil = "SELECT nome FROM perfil WHERE id_perfil = :id_perfil";
     $stmtPerfil = $pdo->prepare($sqlPerfil);
-    $stmtPerfil->bindParam(':id_perfil',$id_perfil);
+    $stmtPerfil->bindParam(':id_perfil', $id_perfil);
     $stmtPerfil->bindParam(':email', $email);
     $stmtPerfil->execute();
     $perfil = $stmtPerfil->fetch(PDO::FETCH_ASSOC);
@@ -27,38 +27,83 @@ if (!isset($_SESSION['usuario'])) {
 
     //Definição das permissoes por perfil
     $permissoes = [
-        1=>["Cadastrar" => ["cadastro_usuario.php",
-            "cadastro_perfil.php" ,"cadastro_cliente.php",
-            "cadastro_fornecedor.php","cadastro_produto.php",
-            "cadastro_funcionario.php"],
-
-            "Buscar" => ["buscar_usuario.php",
-            "buscar_perfil.php","buscar_cliente.php",
-            "buscar_fornecedor.php","buscar_produto.php",
-            "buscar_funcionario.php"],
-
-            "Alterar" => ["alterar_usuario.php",
-            "alterar_perfil.php","alterar_cliente.php",
-            "alterar_fornecedor.php","alterar_produto.php",
-            "alterar_funcionario.php"],
-
-            "Excluir" => ["excluir_usuario.php",
-            "excluir_perfil.php","excluir_cliente.php",
-            "excluir_fornecedor.php","excluir_produto.php",
-            "excluir_funcionario.php"]],
-
-        2=>["Cadastrar" => ["cadastro_cliente.php",
-            "cadastro_fornecedor.php","cadastro_produto.php",
-            "cadastro_funcionario.php"],
-
-           
-
-    ]
+        1 => [
+            "Cadastrar" => [
+                "cadastro_usuario.php",
+                "cadastro_perfil.php",
+                "cadastro_cliente.php",
+                "cadastro_fornecedor.php",
+                "cadastro_produto.php",
+                "cadastro_funcionario.php"
+            ],
+            "Buscar" => [
+                "buscar_usuario.php",
+                "buscar_perfil.php",
+                "buscar_cliente.php",
+                "buscar_fornecedor.php",
+                "buscar_produto.php",
+                "buscar_funcionario.php"
+            ],
+            "Alterar" => [
+                "alterar_usuario.php",
+                "alterar_perfil.php",
+                "alterar_cliente.php",
+                "alterar_fornecedor.php",
+                "alterar_produto.php",
+                "alterar_funcionario.php"
+            ],
+            "Excluir" => [
+                "excluir_usuario.php",
+                "excluir_perfil.php",
+                "excluir_cliente.php",
+                "excluir_fornecedor.php",
+                "excluir_produto.php",
+                "excluir_funcionario.php"
+            ]
+        ],
+        2 => [
+            "Cadastrar" => [
+                "cadastro_cliente.php"
+            ],
+            "Buscar" => [
+                "buscar_cliente.php",
+                "buscar_fornecedor.php",
+                "buscar_produto.php"
+            ],
+            "Alterar" => [
+                "alterar_cliente.php",
+                "alterar_fornecedor.php"
+            ]
+        ],
+        3 => [
+            "Cadastrar" => [
+                "cadastro_fornecedor.php",
+                "cadastro_produto.php"
+            ],
+            "Buscar" => [
+                "buscar_usuario.php",
+                "buscar_perfil.php",
+                "buscar_cliente.php",
+                "buscar_fornecedor.php",
+                "buscar_produto.php",
+                "buscar_funcionario.php"
+            ],
+            "Alterar" => [
+                "alterar_usuario.php",
+                "alterar_perfil.php",
+                "alterar_cliente.php",
+                "alterar_fornecedor.php",
+                "alterar_produto.php",
+                "alterar_funcionario.php"
+            ],
+            "Excluir" => [
+                "excluir_usuario.php",
+                "excluir_perfil.php",
+                "excluir_cliente.php",
+                "excluir_fornecedor.php",
+                "excluir_produto.php",
+                "excluir_funcionario.php"
+            ]
+        ]
+    ];
 }
-
-
-
-
-
-?>
-    
